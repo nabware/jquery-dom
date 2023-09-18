c6159b178563c83b57dca1915169605dd0b5cd48
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-18 16:31:15 -0700

    Made changes per code review

--- movies.js
+++ movies.js
@@ -1,15 +1,20 @@
+"use strict";
+
+/** Appends movie review in DOM with "title: rating <delete button>" */
+
 function submitMovieReview() {
   const $container = $("<div>");
-  const $title = $("#title").val();
-  const $rating = $("#rating").val();
-  const $button = $("<button>", {text: "Delete"});
-  $button.on("click", (evt) => {
-    $(evt.target.parentElement).remove();
-  });
+  const title = $("#title").val();
+  const rating = $("#rating").val();
+  const $button = $("<button>", {text: "Delete"}).addClass("review-delete");
 
-  $container.append(`${$title}: ${$rating} `).append($button);
+  $container.append(`${title}: ${rating} `).append($button);
 
   $("#reviews").append($container);
 }
+
+$("#reviews").on("click", ".review-delete", (evt) => {
+  $(evt.target.parentElement).remove();
+});
 
 $("#submit").on("click", submitMovieReview);
